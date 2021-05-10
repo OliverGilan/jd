5b2a5ff9822c3d45b2fe7f0784dd7cd1c5893f79
Oliver Gilan <olivergilan@gmail.com>
2021-05-10 00:08:24 -0400

    Added nextAvailable method to category

--- models/category.go
+++ models/category.go
@@ -3,5 +3,28 @@ package models
 type Category struct {
 	Code int
 	Name string
-	Items []map[int]string `mapstructure:"items"`
+	Items map[int]string `mapstructure:"items"`
+}
+
+func (c *Category) getNextItemCode() int {
+	code := 1
+	for code < 100 {
+		if _, ok := c.Items[code]; ok {
+			code = code + 1
+			if code == 100{
+				code = -1
+			}
+		}else{
+			break
+		}
+	}
+
+	return code
+}
+
+func (c *Category) isItemCodeAvailable(code int) bool{
+	if _, ok := c.Items[code]; ok {
+		return false
+	}
+	return true
 }
