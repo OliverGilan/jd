54764878c9422e15031454159936cf23355e2779
Oliver Gilan <olivergilan@gmail.com>
2021-05-10 00:06:10 -0400

    Added nextAvailableCode methods to area and category

--- models/area.go
+++ models/area.go
@@ -3,5 +3,28 @@ package models
 type Area struct {
 	Code int
 	Name string
-	Categories []map[int]Category `mapstructure:"categories"`
+	Categories map[int]Category `mapstructure:"categories"`
+}
+
+func (a *Area) getNextCategoryCode() int {
+	code := 1
+	for code < 10 {
+		if _, ok := a.Categories[code]; ok {
+			code = code + 1
+			if code == 10{
+				code = -1
+			}
+		}else{
+			break
+		}
+	}
+
+	return code
+}
+
+func (a *Area) isCategoryCodeAvailable(code int) bool{
+	if _, ok := a.Categories[code]; ok {
+		return false
+	}
+	return true
 }
--- models/projects.go
+++ models/projects.go
@@ -3,5 +3,32 @@ package models
 type Project struct {
 	Code int 
 	Name string
-	Areas []map[int]Area `mapstructure:"areas"`
+	Areas map[int]Area `mapstructure:"areas"`
+}
+
+// func (p *Project) GetAreas() []map[int]Area{
+// 	return p.Areas
+// }
+
+func (p *Project) getNextAreaCode() int {
+	code := 10
+	for code < 100 {
+		if _, ok := p.Areas[code]; ok {
+			code = code + 10
+			if code == 100{
+				code = -1
+			}
+		}else{
+			break
+		}
+	}
+
+	return code
+}
+
+func (p *Project) isAreaCodeAvailable(code int) bool{
+	if _, ok := p.Areas[code]; ok {
+		return false
+	}
+	return true
 }
