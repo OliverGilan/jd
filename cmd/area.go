aeb6f66431f9a74ee88bb9f4c8efb6d0ce428bc5
Oliver Gilan <olivergilan@gmail.com>
2021-05-20 23:08:47 -0400

    Implemented area

--- cmd/area.go
+++ cmd/area.go
@@ -16,10 +16,11 @@ limitations under the License.
 package cmd
 
 import (
-	"fmt"
+	"jd/models"
+	"os"
+	"strconv"
 
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 )
 
 // areaCmd represents the area command
@@ -35,76 +36,54 @@ var areaCmd = &cobra.Command{
 	
 	Delete existing area with -rm.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("area called:")
-		// cwd, _ := os.Getwd()
+		cwd, err := os.Getwd()
+		if err != nil {
+			panic(err)
+		}
 
+		projectCode := config.GetActiveProject(cwd);
+		if projectCode == 0{
+			panic("No active project. You must create at least one project before creating any areas")
+		}
+		project := config.Projects[projectCode]
 
-		// if error := viper.WriteConfig(); error != nil {
-		// 	panic(error)
-		// }
-		var c AreaConfig;
-		viper.UnmarshalKey("projects.100.areas", &c)
-		fmt.Println(c)
+		var nextCode int;
+		if code, err := cmd.Flags().GetInt("code"); err == nil {
+			if !project.IsAreaCodeAvailable(code) {
+				panic("Code is already in use: " + project.Areas[code].Name)
+			}
+			nextCode = code
+		} else {
+			if nc := project.GetNextAreaCode(); nc == -1 {
+				panic("All project codes taken.")
+			} else {
+				nextCode = nc
+			}
+		}
+
+		dirName := strconv.Itoa(nextCode) + "-" + strconv.Itoa(nextCode + 9) + " " + args[0]
+
+		if e := os.Chdir(config.Paths[projectCode]); e != nil {
+			panic(e)
+		}
+		if e := os.Mkdir(dirName, 0755); e != nil {
+			panic(e)
+		}
+
+		area := models.Area{
+			Code: nextCode,
+			Name: dirName,
+			Categories: make(map[int]models.Category, 10),
+		};
+
+		project.Areas[nextCode] = area;
+
+		if e := config.SaveConfig(); e != nil {
+			panic(e)
+		}
+		os.Chdir(cwd)
 	},
 	Args: cobra.MaximumNArgs(2),
-}
-
-type AreaConfig struct {
-	Areas []map[int]string;
-}
-
-func (a AreaConfig) getNextAreaCode() (int, error){
-	return 0, nil
-}
-
-func (a AreaConfig) isCodeAvailable(code int) (bool){
-	// for _, elem := range a.Areas {
-	// 	if
-	// }
-	return true
-}
-
-func getNextAreaCode(projectCode string, cwd string, acode int, err error) (int, error) {
-	// var areaCode int
-	// var projectCode int
-
-	// projectPaths := viper.GetStringMapString("project_paths")
-	// for key, elem := range projectPaths {
-	// 	if strings.Contains(cwd, elem){
-	// 		key, _ := strconv.Atoi(key)
-	// 		projectCode = key
-	// 		break
-	// 	}
-	// }
-	// viper.GetSlice
-	// areaCodes := viper.GetIntSlice(projectCode)
-
-	// if err == nil {
-	// 	if acode >= 10 && acode <= 90 && acode % 10 == 0{
-	// 		if(areaCodes[acode-100] != 0){
-	// 			return -1, errors.New("code in use")
-	// 		}
-	// 		return fcode, nil
-	// 	}else{
-	// 		return -1, errors.New("code must be a 3-digit integer")
-	// 	}
-	// }else{
-	// 	if areaCodes == nil {
-	// 		areaCodes = make([]int, 10)
-	// 		areaCode = 10
-	// 		areaCodes[0] = 10
-	// 	}
-	// 	for i := 0; i < cap(projectCodes); i++{
-	// 		if(projectCodes[i] == 0){
-	// 			code = i + 100
-	// 			projectCodes[i] = code
-	// 			break
-	// 		}
-	// 	}
-	// }
-
-	// viper.Set("projects", projectCodes)
-	return 0, nil
 }
 
 func init() {
